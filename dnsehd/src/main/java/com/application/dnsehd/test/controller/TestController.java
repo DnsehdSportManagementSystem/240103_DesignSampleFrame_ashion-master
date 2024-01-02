dbaaed742bbd9b37b1a5a38a0e8054731436aa39
fusiontwo <yr0955@naver.com>
2024-01-03 04:26:02 +0900

    240103 04:25

---
+++ dnsehd/src/main/java/com/application/dnsehd/test/controller/TestController.java
@@ -0,0 +1,91 @@
+package com.application.dnsehd.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+	// 1. 프로젝트 실행 확인용 view와 맵핑
+	@GetMapping("/test")
+	public String test() {
+		return "designTest/home";
+	}
+	
+	// 2. ashion-master 원본 view와 맵핑
+	@GetMapping("/blog")
+	public String blog() {
+		return "ashion-master_sample/blog";
+	}	
+
+	@GetMapping("/blogDetails")
+	public String blogDetails() {
+		return "ashion-master_sample/blog-details";
+	}	
+	
+	@GetMapping("/checkout")
+	public String checkout() {
+		return "ashion-master_sample/checkout";
+	}	
+	
+	@GetMapping("/contact")
+	public String contact() {
+		return "ashion-master_sample/contact";
+	}	
+	
+	@GetMapping("/index")
+	public String index() {
+		return "ashion-master_sample/index";
+	}	
+	
+	@GetMapping("/productDetails")
+	public String productDetails() {
+		return "ashion-master_sample/product-details";
+	}	
+	
+	@GetMapping("/shop")
+	public String shop() {
+		return "ashion-master_sample/shop";
+	}	
+	
+	@GetMapping("/shop-cart")
+	public String shopCart() {
+		return "ashion-master_sample/shop-cart";
+	}	
+	
+	
+	// 3. 디자인 테스트용으로 수정한 view와 맵핑
+//	@GetMapping("/")
+//	public String () {
+//		return "designTest/";
+//	}	
+
+	@GetMapping("/main")
+	public String main() {
+		return "designTest/main";
+	}	
+	
+	@GetMapping("/class")
+	public String findClass() {
+		return "designTest/class";
+	}	
+
+	@GetMapping("/classDetail")
+	public String classDetail() {
+		return "designTest/classDetail";
+	}	
+
+	@GetMapping("/teacher")
+	public String findTeacher() {
+		return "designTest/teacher";
+	}	
+
+	@GetMapping("/teacherDetail")
+	public String teacherDetail() {
+		return "designTest/teacherDetail";
+	}	
+
+
+	
+	
+}
